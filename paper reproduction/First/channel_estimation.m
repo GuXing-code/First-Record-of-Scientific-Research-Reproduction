aa483e8aa8615cb900ced0f8ac2c510a712e5c21
谷星不良人 <2377715963@qq.com>
2026-04-01 19:00:48 +0800

    加入LMMSE信道估计，完成第二章单用户

--- paper reproduction/First/channel_estimation.m
+++ paper reproduction/First/channel_estimation.m
@@ -4,6 +4,8 @@ function [h_pre] = channel_estimation(know_signal, obverse_signal, L, N_b)
 % L: 需要估计的等效信道长度（单位：符号）
 % N_b: 用于估计的观测符号长度
 
+
+%使用LS估计信道
 know_signal = reshape(know_signal,1,[]);
 obverse_signal = reshape(obverse_signal,1,[]);
 
@@ -16,4 +18,14 @@ for i = 1:N_b
 end
 
 h_pre = (X' * X) \ (X' * Y);
+
+
+%加入LMMSE估计信道
+%1.使用LS得到初步信道估计h_pre
+%2.估计噪声方差
+sigma=sum((Y-X*h_pre).^2)/N_b;
+%3.构建信道协方差——>没有历史数据:R_hh=E单位矩阵
+R_hh=eye(L,L);
+%4.代入LMMSE公式
+h_pre=R_hh*X'*(X*R_hh*X'+sigma*eye(N_b))^(-1)*Y;
 end
